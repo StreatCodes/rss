c37c785b3570259cde7bdaa2a5133aae4cb0c21b
Matt Streatfield <matt.r.streatfield@gmail.com>
2025-04-12 14:45:41 +1000

    Initial search view

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## RSS
+
+Search RSS feeds and collate a feed.
+
+### Build
+
+Run `go run cmd/rss/rss.go` or build `go build cmd/rss/rss.go`
--- cmd/rss/rss.go
+++ cmd/rss/rss.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func render(w http.ResponseWriter, data any, pages ...string) {
+	files := append([]string{"internal/templates/base.tmpl"}, pages...)
+	tmpl := template.Must(template.ParseFiles(files...))
+	tmpl.ExecuteTemplate(w, "base", data)
+}
+
+func homeGET(w http.ResponseWriter, r *http.Request) {
+	render(w, nil, "internal/templates/home.tmpl")
+}
+
+func homePOST(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Posted to home page")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web"))))
+	mux.HandleFunc("GET /", homeGET)
+	mux.HandleFunc("POST /", homePOST)
+
+	addr := ":8080"
+	log.Printf("Server running at %s", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
