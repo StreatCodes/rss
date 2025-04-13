c5d28ad2701eddf2b1c05d0cc3d59240aae93dce
Matt Streatfield <matt.r.streatfield@gmail.com>
2025-04-13 14:38:16 +1000

    Add search result handler Redo templates so they're more re-usable

--- cmd/rss/rss.go
+++ cmd/rss/rss.go
@@ -1,32 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-func render(w http.ResponseWriter, data any, pages ...string) {
-	files := append([]string{"internal/templates/base.tmpl"}, pages...)
-	tmpl := template.Must(template.ParseFiles(files...))
-	tmpl.ExecuteTemplate(w, "base", data)
+type TemplateData struct {
+	Results []string
 }
 
-func homeGET(w http.ResponseWriter, r *http.Request) {
-	render(w, nil, "internal/templates/home.tmpl")
+func render(w http.ResponseWriter, name string, data any) {
+	tmpl := template.Must(template.ParseGlob("internal/templates/*.tmpl"))
+	tmpl.ExecuteTemplate(w, name, data)
 }
 
-func homePOST(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Posted to home page")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, "home", nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	isHtmx := r.Header.Get("HX-Request")
+	searchResult := r.URL.Query().Get("search")
+	if isHtmx == "true" {
+		render(w, "results", []string{searchResult})
+	} else {
+		render(w, "home", TemplateData{Results: []string{searchResult}})
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web"))))
-	mux.HandleFunc("GET /", homeGET)
-	mux.HandleFunc("POST /", homePOST)
+	mux.HandleFunc("GET /", homeHandler)
+	mux.HandleFunc("GET /search", searchHandler)
 
 	addr := ":8080"
 	log.Printf("Server running at %s", addr)
